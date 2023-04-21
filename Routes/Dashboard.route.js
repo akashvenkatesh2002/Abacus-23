ec89382e6cf85d720a4e2a00c76973ceb9e2d92d
akashvenkatesh2002 <akashvenkatesh2002@gmail.com>
2023-04-21 23:51:04 +0530

    My Final Code

--- Routes/Dashboard.route.js
+++ Routes/Dashboard.route.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const router = express.Router()
 
-const controllers = require('../controllers/dashboard.controller')
+const controllers = require('../Controllers/dashboard.controller')
 
 router.post('/', controllers.viewDashboard)
 
+
+router.post('/register/:eventId', controllers.viewDashboard)
+router.post('/register/:workshopId', controllers.viewDashboard)
+
 module.exports = router
+
+
 
 
