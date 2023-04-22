2e51a96f6435ae3fcc815da61887061676c2fed7
harshit0902 <83470228+harshit0902@users.noreply.github.com>
2023-04-23 01:02:06 +0530

    Events done

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(async(req, res, next) => {
 })
 
 app.use((err, req, res, next) => {
-    res.status(err.status || 500)
+    // res.status(err.status || 500)
     res.send( {
         error: {
             status: err.status || 500,
