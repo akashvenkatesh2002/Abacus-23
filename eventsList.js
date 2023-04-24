314e2bc3b3e35f57ea21d3602160a121898f4eba
harshit0902 <83470228+harshit0902@users.noreply.github.com>
2023-04-24 10:56:10 +0530

    Create eventsList.js

---
+++ eventsList.js
@@ -0,0 +1,21 @@
+exports.eventsList =
+{
+    1: "OLPC",
+    2: "OSPC",
+    3: "Codesomniacs",
+    4: "Deadlock Opener",
+    5: "Code with Memes",
+    6: "Booting",
+    7: "Brainstormiz",
+    8: "BugFree",
+    9: "The Guy in the chair",
+    10: "Spotlight",
+    11: "Kolly Quiz",
+    12: "Gamindrome",
+    13: "Eight Square",
+    14: "Alcatraz",
+    15: "IPL Auction",
+    16: "Once Twice Thrice",
+    17: "Design-a-thon",
+    18: "Thadam"
+}
